d1212f4b1d7873ab5b5feab19c1a84ee200b8319
DanyAlbeiroCastroRuiz <danycastro769@gmail.com>
2026-09-05 15:39:03 -0500

    feat: reto 7 completado

--- src/main/java/com/colegiomagico/reto7/CofreVaritas.java
+++ src/main/java/com/colegiomagico/reto7/CofreVaritas.java
@@ -6,7 +6,13 @@ package com.colegiomagico.reto7;
 public class CofreVaritas {
 
     public static int varitaMasPoderosa(int[] tamanos) {
-        return 0; // TODO: Cambia esto
+        int numM=tamanos[0];
+        for(int i=0;i<tamanos.length;i++){
+            if(tamanos[i] != numM && tamanos[i]>numM){
+                numM=tamanos[i];
+            }
+        }
+        return numM;
     }
 
     public static void main(String[] args) {
