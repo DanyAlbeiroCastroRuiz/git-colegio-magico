8e26b0ec165670bff9b6369968a6262b3cef7ba9
DanyAlbeiroCastroRuiz <danycastro769@gmail.com>
2026-09-05 14:47:25 -0500

    feat: reto 1 completado

--- src/main/java/com/colegiomagico/reto1/HechizoIniciacion.java
+++ src/main/java/com/colegiomagico/reto1/HechizoIniciacion.java
@@ -6,7 +6,7 @@ package com.colegiomagico.reto1;
 public class HechizoIniciacion {
 
     public static String invocar() {
-        return ""; // TODO: Cambia esto
+        return "Dany Castro - Sebastian";
     }
 
     public static void main(String[] args) {
