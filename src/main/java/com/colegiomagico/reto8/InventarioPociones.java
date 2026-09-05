decec44b96675046e94f709e1b22a09162c36a15
DanyAlbeiroCastroRuiz <danycastro769@gmail.com>
2026-09-05 15:52:06 -0500

    feat: reto 8 completado

--- src/main/java/com/colegiomagico/reto8/InventarioPociones.java
+++ src/main/java/com/colegiomagico/reto8/InventarioPociones.java
@@ -1,12 +1,19 @@
 package com.colegiomagico.reto8;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Filtra la lista de ingredientes devolviendo solo aquellos que empiezan con 'A'.
  */
 public class InventarioPociones {
 
     public static java.util.List<String> filtrar(java.util.List<String> ingredientes) {
-        return new java.util.ArrayList<>(); // TODO: Cambia esto
+        
+        java.util.List<String> resultado = ingredientes.stream()
+            .filter(n -> n.toLowerCase().startsWith("a"))
+            .collect(Collectors.toList());
+            return resultado;
     }
 
     public static void main(String[] args) {
